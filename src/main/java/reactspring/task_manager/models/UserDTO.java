e5266f38604d896f853c68d39e427d8c0f5ed4cc
mukhtarkhanov25@gmail.com <mukhtarkhanov25@gmail.com>
2021-03-15 09:36:40 +0600

    JwtToken

---
+++ src/main/java/reactspring/task_manager/models/UserDTO.java
@@ -0,0 +1,20 @@
+package reactspring.task_manager.models;
+
+import reactspring.task_manager.entities.Roles;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+
+    private Long id;
+    private String email;
+    private List<Roles> roles;
+
+}
